bdc3fd3460a374778d745b2c92fe249f7d5a5267
LuisLDA <luisldacr@gmail.com>
2023-05-11 19:46:40 -0500

    Agregado slideshow

--- lib/domain/datasources/movies_datasource.dart
+++ lib/domain/datasources/movies_datasource.dart
@@ -1,5 +1,5 @@
 import '../entities/movie.dart';
 
-abstract class MovieDataSource {
+abstract class MoviesDataSource {
   Future<List<Movie>> getNowPlaying({int page = 1});
 }
---
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -0,0 +1,24 @@
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/movie_db.dart';
+
+class MovieMapper {
+  static Movie movieDBtoEntity(MovieMovieDB moviedb) => Movie(
+      adult: moviedb.adult,
+      backdropPath: (moviedb.backdropPath != '')
+          ? 'https://image.tmdb.org/t/p/w500${moviedb.backdropPath}'
+          : 'https://png.pngtree.com/thumb_back/fw800/background/20220119/pngtree-system-error-404-page-not-found-image_983764.jpg',
+      genreIds: moviedb.genreIds.map((e) => e.toString()).toList(),
+      id: moviedb.id,
+      originalLanguage: moviedb.originalLanguage,
+      originalTitle: moviedb.originalTitle,
+      overview: moviedb.overview,
+      popularity: moviedb.popularity,
+      posterPath: (moviedb.posterPath != '')
+          ? 'https://image.tmdb.org/t/p/w500${moviedb.posterPath}'
+          : 'no-image',
+      releaseDate: moviedb.releaseDate,
+      title: moviedb.title,
+      video: moviedb.video,
+      voteAverage: moviedb.voteAverage,
+      voteCount: moviedb.voteCount);
+}
