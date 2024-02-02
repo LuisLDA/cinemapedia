7fd724282638b157cf524b148a708a1816adfeac
LuisLDA <luisldacr@gmail.com>
2024-02-01 23:12:49 -0500

    agregado mas fuentes de tmdb

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,8 +1,8 @@
-import 'package:cinemapedia/config/constants/environment.dart';
-import 'package:cinemapedia/presentation/providers/movies/movies_providers.dart';
 import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../providers/providers.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -13,6 +13,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: _HomeView(),
+      bottomNavigationBar: const CustomBottomNavigation(),
     );
   }
 }
@@ -27,20 +28,80 @@ class _HomeViewState extends ConsumerState<_HomeView> {
   void initState() {
     super.initState();
     ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
+    ref.read(popularMoviesProvider.notifier).loadNextPage();
+    ref.read(ratedMoviesProvider.notifier).loadNextPage();
+    ref.read(upcomingMoviesProvider.notifier).loadNextPage();
   }
 
   @override
   Widget build(BuildContext context) {
+
+
+    final initialLoading = ref.watch(initialLoadingProvider);
+
+    if (initialLoading) {
+      return const FullScreenLoader();
+    }
+
+    final slideShowMovies = ref.watch(moviesSlideshowProvider);
     final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
+    final popularMovies = ref.watch(popularMoviesProvider);
+    final upcomingMovies = ref.watch(upcomingMoviesProvider);
+    final ratedMovies = ref.watch(ratedMoviesProvider);
 
-    if (nowPlayingMovies.isEmpty)
-      return const Center(child: CircularProgressIndicator());
 
-    return Column(
-      children: [
-        CustomAppBar(),
-        MoviesSlideshow(movies: nowPlayingMovies)
-      ],
+    return Visibility(
+      child: CustomScrollView(slivers: [
+        const SliverAppBar(
+          floating: true,
+          flexibleSpace: FlexibleSpaceBar(
+            title: CustomAppBar(),
+            centerTitle: true,
+          )
+        ),
+    
+        SliverList(
+            delegate: SliverChildBuilderDelegate((context, index) {
+          return Column(
+            children: [
+              //const CustomAppBar(),
+              MoviesSlideshow(movies: slideShowMovies),
+              MovieHorizontalListview(
+                movies: nowPlayingMovies,
+                title: 'En cines',
+                subTitle: 'Hoy',
+                loadNextPage: () {
+                  ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
+                },
+              ),
+              MovieHorizontalListview(
+                movies: upcomingMovies,
+                title: 'Proximamente',
+                subTitle: 'En este mes',
+                loadNextPage: () {
+                  ref.read(upcomingMoviesProvider.notifier).loadNextPage();
+                },
+              ),
+              MovieHorizontalListview(
+                movies: popularMovies,
+                title: 'Populares',
+                subTitle: 'En este mes',
+                loadNextPage: () {
+                  ref.read(popularMoviesProvider.notifier).loadNextPage();
+                },
+              ),
+              MovieHorizontalListview(
+                movies: ratedMovies,
+                title: 'Rating',
+                subTitle: 'En este mes',
+                loadNextPage: () {
+                  ref.read(ratedMoviesProvider.notifier).loadNextPage();
+                },
+              ),
+            ],
+          );
+        }, childCount: 1))
+      ]),
     );
   }
 }
---
+++ lib/presentation/widgets/shared/full_screen_loader.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class FullScreenLoader extends StatelessWidget {
+  const FullScreenLoader({super.key});
+
+  Stream<String> getLoadingMessages() {
+    final messages = <String>[
+      'Cargando peliculas',
+      'Cargando populares',
+      'En un momentooo',
+      'Esto esta tardando mucho 😅',
+    ];
+    return Stream.periodic(
+        const Duration(milliseconds: 1200), (step) => messages[step]).take(messages.length);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('Espere...'),
+          const SizedBox(height: 10),
+          const CircularProgressIndicator(
+            strokeWidth: 2,
+          ),
+          const SizedBox(height: 10),
+          StreamBuilder(
+            stream: getLoadingMessages(),
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) {
+                return const Text('Cargando...');
+              }
+              return Text(snapshot.data!);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
